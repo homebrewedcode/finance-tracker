91ccdddaa0fb82cec65a153b9bbd93f455316b8f
Dale Pollitt <dale.negativezero@gmail.com>
2017-07-16 13:48:30 -0700

    Add devise and bootstrap for authentication and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,7 +55,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  #devise_for :users
 
-  root 'welcome#index'
+
 
 end
